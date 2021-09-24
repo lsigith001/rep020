29f4b09739af1b6709be21269dd7bfcc5672e4bb
unknown <lsiccha@yahoo.com>
2021-09-24 03:07:51 +0200

    hallo

--- test1.py
+++ test1.py
@@ -1,8 +1,17 @@
 def printList():
-	list0=list0st()
-	for i in range(1,21):
-		list0.append(i**2)
-	print list0[:5]  # änderung 01 lokal
-		
+    list0 = list()
+    for i in range(1, 21):
+        list0.append(i ** 2)
+    print(list0[:5])  # änderung 01 lokal
+
+
 # aenderung in git
 printList()
+
+# -------
+
+var1 = 11.00
+var2 = 22.00
+print(f"jsjsjsjsj  {  var1}  ....  {var2}")
+
+
